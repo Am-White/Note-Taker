90e8d8248a2e1e20e0d138cdbef4aaade9bbfc94
littlefire <aybwhite@gmail.com>
2021-01-10 14:25:49 -0800

    added comments

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,24 +1,33 @@
 var fs = require('fs');
 
+//Read and parse notes data
 var data= fs.readFileSync("db/db.json");
 var notesData = JSON.parse(data);
 
 module.exports = function(app) {
+    //Read the db.json file and return
     app.get("/api/notes", function(req,res) {
         res.json(notesData);
     });
 
+    //Receive notes to save
     app.post("/api/notes", function(req,res) {
+
         var newNotes = req.body;
+        //Meta-character
         newNotes.id = newNotes.title.replace(/\s+/g, "").toLowerCase();
+
         notesData.push(newNotes);
         fs.writeFileSync("db/db.json", JSON.stringify(notesData));
         return res.json(newNotes);
     });
 
+    //Deleted notes with given ID
     app.delete("/api/notes/:id", function(req,res) {
+
         var chosen = req.params.id;
         notesData = notesData.filter(({ id }) => id != chosen);
+
         fs.writeFileSync("db/db.json", JSON.stringify(notesData));
         return res.json(true);
     });
