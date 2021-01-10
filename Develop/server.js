40cd18dd505dcb690c456b8bf751fcf1560267ab
littlefire <aybwhite@gmail.com>
2021-01-10 14:07:37 -0800

    api routes.js codes joingin html GET requests

--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Route files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
