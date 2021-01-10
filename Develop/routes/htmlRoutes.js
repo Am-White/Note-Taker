c29747710cefa0af24b78e0b785429c428f2f73f
littlefire <aybwhite@gmail.com>
2021-01-10 14:26:49 -0800

    more comments

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,10 +1,13 @@
 var path = require('path');
 
+//HTML GET request
 module.exports = function(app) {
+    //Notes page
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
+    //Start page
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
